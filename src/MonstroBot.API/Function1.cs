a7b0b61681185daadfff713f33ee4327dd06cc28
Hank McCord <hymccord@gmail.com>
2024-05-09 17:13:24 -0400

    Use test function to see environment

--- src/MonstroBot.API/Function1.cs
+++ src/MonstroBot.API/Function1.cs
@@ -18,7 +18,13 @@ namespace MonstroBot.API
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            return new OkObjectResult($"Welcome to {GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")} Azure Functions!");
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            return name + ": " +
+                System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
         }
     }
 }
