b27928dc2a81a2b545c99286dc10979367f5c6ba
Hank McCord <hymccord@gmail.com>
2024-05-16 14:00:16 -0400

    Prototype crown count endpoint

---
+++ src/MonstroBot.API/CrownHttpTrigger.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Extensions.Logging;
+
+namespace MonstroBot.API
+{
+    public class CrownHttpTrigger
+    {
+        private readonly ILogger<CrownHttpTrigger> _logger;
+        private readonly OpenApiSettings _openApi;
+        private readonly MouseHuntHtmlClient _htmlClient;
+
+        public CrownHttpTrigger(ILogger<CrownHttpTrigger> logger, OpenApiSettings openApi, MouseHuntHtmlClient mouseHuntHtmlClient)
+        {
+            _logger = logger;
+            _openApi = openApi;
+            _htmlClient = mouseHuntHtmlClient;
+        }
+
+        [Function(nameof(GetCrownCount))]
+        public async Task<IActionResult> GetCrownCount([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "crowncount/{snuid}")] HttpRequest req,
+            string snuid)
+        {
+            var crowns = await _htmlClient.KingsCrownsAsync(snuid);
+
+            return new OkObjectResult(crowns);
+        }
+    }
+}
