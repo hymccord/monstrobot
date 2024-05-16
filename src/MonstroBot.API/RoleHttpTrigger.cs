5e6ef8c3a55fd648fe5deb8d5651ede01d7f565e
Hank McCord <hymccord@gmail.com>
2024-05-16 16:18:58 -0400

    Also make role check more intuitive

--- src/MonstroBot.API/RoleHttpTrigger.cs
+++ src/MonstroBot.API/RoleHttpTrigger.cs
@@ -56,7 +56,7 @@ public class RoleHttpTrigger(ILogger<RoleHttpTrigger> logger, OpenApiSettings op
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(RoleCheckResponse), Summary = "successful operation", Description = "successful operation")]
     [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Description = "Invalid ID supplied")]
     [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.Unauthorized, Description = "Supplied credentials are invalid or expired")]
-    public async Task<IActionResult> CheckRole([HttpTrigger(AuthorizationLevel.Anonymous, ["POST"], Route = "role/{role}/{id}")] HttpRequest req,
+    public async Task<IActionResult> CheckRole([HttpTrigger(AuthorizationLevel.Anonymous, ["POST"], Route = "role/{id}/{role}")] HttpRequest req,
         [FromBody] MouseHuntAuth account,
         string role,
         ulong id)
