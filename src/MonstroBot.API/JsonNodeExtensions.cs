c2467bf93afd897e44c9b249d691f6d6920b0e28
Hank McCord <hymccord@gmail.com>
2024-05-17 16:23:35 -0400

    Use JSONPath to simplify json querying

---
+++ src/MonstroBot.API/JsonNodeExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using Json.Path;
+
+namespace MonstroBot.API;
+
+public static class JsonNodeExtensions
+{
+    /// <summary>
+    /// Select and extract JSON values from within a given JSON value using JSONPath
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="jsonPathQuery"></param>
+    /// <returns></returns>
+    /// <exception cref="JsonException"></exception>
+    public static JsonNode Query(this JsonNode node, string jsonPathQuery)
+    {
+        var jsonPath = JsonPath.Parse(jsonPathQuery);
+        var pathResult = jsonPath.Evaluate(node);
+
+        return pathResult.Matches?.SingleOrDefault()?.Value
+            ?? throw new JsonException(string.Format("Couldn't get match from given JSONPath expression: {0}", jsonPathQuery));
+    }
+
+    /// <summary>
+    /// Select and extract JSON values from within a given JSON value using JSONPath
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="jsonPathQuery"></param>
+    /// <returns></returns>
+    public static JsonNode[]? QueryAll(this JsonNode node, string jsonPathQuery)
+    {
+        var jsonPath = JsonPath.Parse(jsonPathQuery);
+        var pathResult = jsonPath.Evaluate(node);
+
+        if (pathResult.Matches?.Count == 0)
+        {
+            return null;
+        }
+
+        return [..pathResult.Matches!.Select(n => n.Value)];
+    }
+}
--- src/MonstroBot.API/MouseHuntApiClient.cs
+++ src/MonstroBot.API/MouseHuntApiClient.cs
@@ -1,6 +1,11 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using Json.Path;
+
+using Microsoft.Extensions.Logging;
 
 using MonstroBot.Models;
 
@@ -13,13 +18,15 @@ public class MouseHuntApiClient
         new ("sn", "Hitgrab"),
         new ("hg_is_ajax", "1")
     ];
+    private readonly ILogger<MouseHuntApiClient> _logger;
     private readonly HttpClient _httpClient;
 
-    public MouseHuntApiClient()
+    public MouseHuntApiClient(ILogger<MouseHuntApiClient> logger)
     {
         var handler = new SocketsHttpHandler()
         {
             AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.Brotli,
+            UseCookies = false,
         };
 
         _httpClient = new HttpClient(handler)
@@ -30,52 +37,40 @@ public class MouseHuntApiClient
         _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("monstro-bot", "1.0"));
         _httpClient.DefaultRequestHeaders.Add("Accept", "application/json, text/javascript, */*; q=0.01");
         _httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+        _logger = logger;
     }
 
     public async Task<string> GetUserSnuid(MouseHuntAuth credentials, ulong userId)
     {
-        JsonDocument doc = await SendRequestAsync(credentials, "/managers/ajax/pages/friends.php", [
-            new ("action", "community_search_by_id"),
-            new ("user_id", $"{userId}"),
+        var doc = await SendRequestAsync(credentials, "/managers/ajax/pages/friends.php", [
+            ("action", "community_search_by_id"),
+            ("user_id", $"{userId}"),
         ]);
 
-        return doc.RootElement
-            .GetProperty("friend")
-            .GetProperty("sn_user_id")
-            .GetString() ?? string.Empty;
+        return (string?)doc.Query("$.friend.sn_user_id") ?? string.Empty;
     }
 
     public async Task<bool> IsEggMaster(MouseHuntAuth credentials, string snUserId)
     {
-        JsonDocument doc = await SendRequestAsync(credentials, "/managers/ajax/users/userData.php", [
-            new ("sn_user_ids[]", snUserId),
-            new ("fields[]", "is_egg_master")
+        var doc = await SendRequestAsync(credentials, "/managers/ajax/users/userData.php", [
+            ("sn_user_ids[]", snUserId),
+            ("fields[]", "is_egg_master")
             ]);
 
-        return doc.RootElement
-            .GetProperty("user_data")
-            .GetProperty(snUserId)
-            .GetProperty("is_egg_master")
-            .GetBoolean();
+        return (bool)doc.Query($"$.user_data['{snUserId}'].is_egg_master");
     }
 
     public async Task<bool> IsCheckmarked(MouseHuntAuth credentials, string snUserId)
     {
         var doc = await SendRequestAsync(credentials, "/managers/ajax/pages/page.php", [
-            new ("page_class", "HunterProfile"),
-            new ("page_arguments[legacyMode]", ""),
-            new ("page_arguments[tab]", "items"),
-            new ("page_arguments[sub_tab]", "false"),
-            new ("page_arguments[snuid]", snUserId),
+            ("page_class", "HunterProfile"),
+            ("page_arguments[legacyMode]", ""),
+            ("page_arguments[tab]", "items"),
+            ("page_arguments[sub_tab]", "false"),
+            ("page_arguments[snuid]", snUserId),
         ]);
 
-        var items = doc.RootElement
-            .GetProperty("page")
-            .GetProperty("tabs")
-            .GetProperty("items")
-            .GetProperty("subtabs")[0]
-            .GetProperty("items")
-            .GetProperty("categories")
+        var items = doc.Query("$.page.tabs.items.subtabs[0].items.categories")
             .Deserialize<ItemCategoryCompletion[]>(JsonSerializerOptionsProvider.Default);
 
         if (items is null)
@@ -89,20 +84,14 @@ public class MouseHuntApiClient
     public async Task<bool> IsCrowned(MouseHuntAuth credentials, string snUserId)
     {
         var doc = await SendRequestAsync(credentials, "/managers/ajax/pages/page.php", [
-            new ("page_class", "HunterProfile"),
-            new ("page_arguments[legacyMode]", ""),
-            new ("page_arguments[tab]", "kings_crowns"),
-            new ("page_arguments[sub_tab]", "false"),
-            new ("page_arguments[snuid]", snUserId),
+            ("page_class", "HunterProfile"),
+            ("page_arguments[legacyMode]", ""),
+            ("page_arguments[tab]", "kings_crowns"),
+            ("page_arguments[sub_tab]", "false"),
+            ("page_arguments[snuid]", snUserId),
         ]);
 
-        var items = doc.RootElement
-            .GetProperty("page")
-            .GetProperty("tabs")
-            .GetProperty("kings_crowns")
-            .GetProperty("subtabs")[0]
-            .GetProperty("mouse_crowns")
-            .GetProperty("badge_groups")
+        var items = doc.Query("$.page.tabs.kings_crowns.subtabs[0].mouse_crowns.badge_groups")
             .Deserialize<MouseCrownBadgeGroup[]>(JsonSerializerOptionsProvider.Default);
 
         if (items is null)
@@ -116,20 +105,14 @@ public class MouseHuntApiClient
     public async Task<bool> IsStarred(MouseHuntAuth credentials, string snUserId)
     {
         var doc = await SendRequestAsync(credentials, "/managers/ajax/pages/page.php", [
-            new ("page_class", "HunterProfile"),
-            new ("page_arguments[legacyMode]", ""),
-            new ("page_arguments[tab]", "mice"),
-            new ("page_arguments[sub_tab]", "location"),
-            new ("page_arguments[snuid]", snUserId),
+            ("page_class", "HunterProfile"),
+            ("page_arguments[legacyMode]", ""),
+            ("page_arguments[tab]", "mice"),
+            ("page_arguments[sub_tab]", "location"),
+            ("page_arguments[snuid]", snUserId),
         ]);
 
-        var items = doc.RootElement
-            .GetProperty("page")
-            .GetProperty("tabs")
-            .GetProperty("mice")
-            .GetProperty("subtabs")[1]
-            .GetProperty("mouse_list")
-            .GetProperty("categories")
+        var items = doc.Query("$.page.tabs.mice.subtabs[1].mouse_list.categories")
             .Deserialize<ItemCategoryCompletion[]>(JsonSerializerOptionsProvider.Default);
 
         if (items is null)
@@ -143,71 +126,102 @@ public class MouseHuntApiClient
     public async Task<IReadOnlyList<CorkboardMessage>> GetCorkboardMessages(MouseHuntAuth credentials, string snuid, int limit = 1)
     {
         var doc = await GetPageAsync<MessageBoardView>(credentials, [
-            new ("page_class", "HunterProfile"),
-            new ("page_arguments[snuid]", snuid),
+            ("page_class", "HunterProfile"),
+            ("page_arguments[snuid]", snuid),
         ],
-        (element) => element
-            .GetProperty("tabs")
-            .GetProperty("profile")
-            .GetProperty("subtabs")[0]
-            .GetProperty("message_board_view"),
+        "$.tabs.profile.subtabs[0].message_board_view",
         JsonSerializerOptionsProvider.RelaxedDateTime);
-
-        if (doc is null)
-        {
-            throw new InvalidOperationException("Couldn't retrieve message_board_view");
-        }
 
         return [..doc.Messages.Take(limit)];
     }
 
-    private async Task<T?> GetPageAsync<T>(MouseHuntAuth credentials, IEnumerable<KeyValuePair<string, string>> parameters, JsonSerializerOptions? jsonSerializerOptions = null)
+    private async Task<T?> GetPageAsync<T>(MouseHuntAuth credentials, IEnumerable<(string key, string value)> parameters, JsonSerializerOptions? jsonSerializerOptions = null)
     {
         var response = await SendRequestAsync(credentials, "/managers/ajax/pages/page.php", parameters);
-        return response.RootElement.GetProperty("page").Deserialize<T>(jsonSerializerOptions ?? JsonSerializerOptionsProvider.Default);
+        return response["page"].Deserialize<T>(jsonSerializerOptions ?? JsonSerializerOptionsProvider.Default);
     }
 
-    private async Task<T?> GetPageAsync<T>(MouseHuntAuth credentials,IEnumerable<KeyValuePair<string, string>> parameters,
-        Func<JsonElement, JsonElement> elementSelector,
-        JsonSerializerOptions? jsonSerializerOptions = null)
+    private async Task<T?> GetPageAsync<T>(MouseHuntAuth credentials, IEnumerable<(string key, string value)> parameters,
+        string jsonPath,
+        JsonSerializerOptions? jsonSerializerOptions = null) where T : class
     {
         var response = await SendRequestAsync(credentials, "/managers/ajax/pages/page.php", parameters);
-        var page = response.RootElement.GetProperty("page");
+        var page = response["page"]!;
 
-        return elementSelector(page).Deserialize<T>(jsonSerializerOptions ?? JsonSerializerOptionsProvider.Default);
+        return page.Query(jsonPath).Deserialize<T>(jsonSerializerOptions ?? JsonSerializerOptionsProvider.Default);
     }
 
-    private async Task<JsonDocument> SendRequestAsync(MouseHuntAuth credentials, string relativeUri, IEnumerable<KeyValuePair<string, string>> parameters)
+    private async Task<JsonNode> SendRequestAsync(MouseHuntAuth credentials, string relativeUri, IEnumerable<(string key, string value)> parameters)
     {
-        var content = new FormUrlEncodedContent(
-            Enumerable.Empty<KeyValuePair<string, string>>()
-                .Concat(_defaultFormData)
-                .Concat(parameters)
-                .Concat([new KeyValuePair<string, string>("uh", credentials.UniqueHash)])
-            );
+        var returnResponse = await RequestAsync();
 
-        var request = new HttpRequestMessage
+        // Session sometimes expires when HG_TOKEN isn't used (seems to be > 1hr)
+        if (IsSessionExpired(returnResponse))
         {
-            RequestUri = new Uri(relativeUri, UriKind.Relative),
-            Method = HttpMethod.Post,
-            Content = content
-        };
-
-        request.Headers.Add("Cookie", $"HG_TOKEN={credentials.HgToken}");
-        request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded")
-        {
-            CharSet = "UTF-8"
-        };
-
-        HttpResponseMessage response = await _httpClient.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-
-        if (response.Content.Headers.ContentType?.MediaType == "text/html")
-        {
-            throw new ArgumentException("Supplied credentials didn't work", nameof(credentials));
+            _logger.LogInformation("Session expired for {UniqueHash}. Refreshing.", credentials.UniqueHash);
+            await RefreshSession();
+            returnResponse = await RequestAsync();
         }
 
-        return await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+        return returnResponse;
+
+        async Task<JsonNode> RequestAsync()
+        {
+            var content = new FormUrlEncodedContent(
+                Enumerable.Empty<KeyValuePair<string, string>>()
+                    .Concat(_defaultFormData)
+                    .Concat(parameters.Select(t => new KeyValuePair<string, string>(t.key, t.value)))
+                    .Concat([new KeyValuePair<string, string>("uh", credentials.UniqueHash)])
+                );
+
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(relativeUri, UriKind.Relative),
+                Method = HttpMethod.Post,
+                Content = content
+            };
+
+            request.Headers.Add("Cookie", $"HG_TOKEN={credentials.HgToken}");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded")
+            {
+                CharSet = "UTF-8"
+            };
+
+            HttpResponseMessage response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            if (response.Content.Headers.ContentType?.MediaType == "text/html")
+            {
+                throw new ArgumentException("Supplied credentials didn't work", nameof(credentials));
+            }
+
+            var node = await JsonNode.ParseAsync(await response.Content.ReadAsStreamAsync().ConfigureAwait(false)).ConfigureAwait(false);
+            if (node is null)
+            {
+                throw new InvalidDataException("Deserialized JSON is null");
+            }
+
+            return node;
+        }
+
+        async Task RefreshSession()
+        {
+            HttpRequestMessage req = new(HttpMethod.Get, new Uri("/camp.php", UriKind.Relative));
+            req.Headers.Add("Cookie", $"HG_TOKEN={credentials.HgToken}");
+
+            HttpResponseMessage response = await _httpClient.SendAsync(req);
+            response.EnsureSuccessStatusCode();
+
+            return;
+        }
+
+        bool IsSessionExpired(JsonNode node)
+        {
+            var path = JsonPath.Parse("$.messageData.popup.messages[0].messageData.body");
+            var matches = path.Evaluate(node).Matches;
+
+            return matches?.Count > 0 && ((string?)matches[0].Value) == "Your session has expired.";
+        }
     }
 
     record ItemCategoryCompletion
