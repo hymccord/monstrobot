9ef66dda9dc0f5f328479462b8e2af2696929914
Hank McCord <hymccord@gmail.com>
2024-05-19 12:46:19 -0400

    Add snuid endpoint to user route

--- src/MonstroBot.API/Functions/UserHttpTrigger.cs
+++ src/MonstroBot.API/Functions/UserHttpTrigger.cs
@@ -1,5 +1,4 @@
-﻿using System.Data;
-using System.Net;
+﻿using System.Net;
 using System.Web.Http;
 
 using Microsoft.AspNetCore.Http;
@@ -11,8 +10,6 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
-using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 
 using MonstroBot.Models;
 using MonstroBot.API.Client;
@@ -91,5 +88,46 @@ public class UserHttpTrigger(ILogger<RoleHttpTrigger> logger, OpenApiSettings op
             _logger.LogError(ex, "Caught general exception while getting corkboard messages");
             return new InternalServerErrorResult();
         }
+    }
+
+    [Function(nameof(GetSnuid))]
+    //[OpenApiOperation("GetSnuid", tags: ["role"], Summary = "Get profile snuid", Description = "Gets snuid from id.", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiParameter(name: "hgToken", In = ParameterLocation.Header, Required = true, Type = typeof(string), Summary = "MouseHunt session token", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiParameter(name: "uniqueHash", In = ParameterLocation.Header, Required = true, Type = typeof(string), Summary = "MouseHunt profile unique hash", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiParameter(name: nameof(id), In = ParameterLocation.Path, Required = true, Type = typeof(ulong), Summary = "MHID of user to return", Description = "MouseHunt profile ID of user to return", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Summary = "successful operation", Description = "successful operation")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Description = "Invalid ID supplied")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.Unauthorized, Description = "Supplied credentials are invalid or expired")]
+    public async Task<IActionResult> GetSnuid([HttpTrigger(AuthorizationLevel.Anonymous, "GET", "POST", Route = "user/{id}/snuid")] HttpRequest req,
+        ulong id)
+    {
+        _logger.LogInformation("document title: {DocumentTitle}", _openApi.DocTitle);
+        _logger.LogInformation("Get snuid of {Id}.", id);
+
+        MouseHuntAuth? account = MouseHuntAuthService.GetCredentialsFromHeader(req.Headers);
+        if (account is null)
+        {
+            return new BadRequestObjectResult("No header credentials supplied.");
+        }
+
+        string snuid = string.Empty;
+        try
+        {
+            snuid = await _apiClient.GetUserSnuid(account, id).ConfigureAwait(false);
+        }
+        // bad creds
+        catch (ArgumentException ex)
+        {
+            _logger.LogWarning(ex, "Error occurred converting MHID {Id} into SNUID", id);
+            return new UnauthorizedObjectResult("Invalid or expired credentials.");
+        }
+        // bad snuid
+        catch (InvalidOperationException ex)
+        {
+            _logger.LogWarning(ex, "Error occurred converting MHID {Id} into SNUID", id);
+            return new BadRequestObjectResult($"Error converting MHID {id} into SNUID.");
+        }
+
+        return new OkObjectResult(snuid);
     }
 }
